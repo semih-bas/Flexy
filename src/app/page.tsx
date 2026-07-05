eed148c000167331af283db3be1aa74c6d58eafa
semih-bas <semihbas2222@gmail.com>
2026-07-05 14:58:09 +0300

    chore: initialize Flexy project

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/layout/Navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+
+      <section>
+        <h1>Flexy v2</h1>
+        <p>
+          Haftalık antrenman planını oluştur, takip et ve gelişimini daha düzenli yönet.
+        </p>
+      </section>
+    </main>
+  );
+}
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav>
+      Flexy v2
+    </nav>
+  );
+}
